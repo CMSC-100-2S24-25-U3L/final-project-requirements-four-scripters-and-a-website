8906d48da05a3f7cefdd840c104942aedf87e28f
marcjoemill <mpmendoza6@up.edu.ph>
2025-04-26 02:46:57 +0800

    chore: setup cloud database and remove node_modules

--- mongoose.js
+++ mongoose.js
@@ -1,20 +1,18 @@
-import mongoose from "mongoose";  // enables MongoDB connections
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-// connection string for the local database called 'project_practice'
-const MONGO_URI = "mongodb://localhost:27017/project_practice";
+// Use full MongoDB URI from .env
+const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,  // parses the MONGO_URI so that the code can understand where we want to go
-  useUnifiedTopology: true, // engine that has better connections maipulation and monitoring
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 });
 
-const db = mongoose.connection; // gives access to the mongoose connection
+const db = mongoose.connection;
 
-// listens for event continously, if there is an error, print message
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("error", console.error.bind(console, "❌ MongoDB connection error:"));
+db.once("open", () => console.log("✅ Connected to MongoDB Atlas!"));
 
-// listen for event once and then print out seccess message
-db.once("open", () => console.log("Connected to MongoDB"));
-
-// Export mongoose as default
 export default mongoose;
